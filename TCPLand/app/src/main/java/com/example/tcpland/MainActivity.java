56599e2e175a8f89ab8aa5e9fc06d97824be7ff0
Tien Emil <63142587+tienem3243@users.noreply.github.com>
2022-12-07 11:57:57 +0700

    done nav and tabbar

--- TCPLand/app/src/main/java/com/example/tcpland/MainActivity.java
+++ TCPLand/app/src/main/java/com/example/tcpland/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.tcpland;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.example.tcpland.Adapter.UserFragmentStateAdapter;
 import com.example.tcpland.Anim.DepthPageTransformer;
@@ -12,38 +17,39 @@ import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.viewpager2.widget.ViewPager2;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Fragment {
 
     private ViewPager2 viewPager2;
     private TabLayout tabLayout;
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityNavigatorBinding binding;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        TabBar();
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.activity_main,container,false);
 
-
-    private void TabBar() {
-        this.viewPager2=findViewById(R.id.viewPager2_employee);
+        this.viewPager2=v.findViewById(R.id.viewPager2_employee);
         // Employee FragmentStateAdapter.
-        UserFragmentStateAdapter adapter = new UserFragmentStateAdapter(this);
+        UserFragmentStateAdapter adapter = new UserFragmentStateAdapter(getActivity());
 
         this.viewPager2.setAdapter(adapter);
         //Page Transform
         // Api 21+.
         this.viewPager2.setPageTransformer(new DepthPageTransformer());
-        TabLayout tabLayout =findViewById(R.id.tabLayout);
+        TabLayout tabLayout =v.findViewById(R.id.tabLayout);
         new TabLayoutMediator(tabLayout, viewPager2,
                 (tab, position) -> {
                     switch (position){
@@ -56,5 +62,10 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
         ).attach();
+        return v;
+    }
+
+    private void TabBar() {
+
     }
 }
